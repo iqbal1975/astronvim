24328e6072f991ed30b26b14daacda63d79ae8d2
Iqbal Thaker <iqbal.thaker@gmail.com>
2023-03-29 12:02:41 +0530

    Removed wakatime

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -91,8 +91,8 @@ return {
       }
     end,
   },
-  { "willothy/flatten.nvim", lazy = false, priority = 1001, opts = { window = { open = "vsplit" } } },
+  { "willothy/flatten.nvim",   lazy = false,            priority = 1001, opts = { window = { open = "vsplit" } } },
   { "junegunn/vim-easy-align", event = "User AstroFile" },
-  { "machakann/vim-sandwich", event = "User AstroFile" },
-  { "wakatime/vim-wakatime", event = "User AstroFile" },
+  { "machakann/vim-sandwich",  event = "User AstroFile" },
+  -- { "wakatime/vim-wakatime", event = "User AstroFile" },
 }
