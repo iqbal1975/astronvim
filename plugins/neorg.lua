b34fffa95d91e2ac8188437128a9389c44b55a3e
Iqbal Thaker <iqbal.thaker@gmail.com>
2023-05-27 11:07:30 +0530

    Added neorg.lua

--- plugins/neorg.lua
+++ plugins/neorg.lua
@@ -5,16 +5,16 @@ return {
   event = "VeryLazy",
   opts = {
     load = {
-      ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.keybinds"] = {}, -- Adds default keybindings
-      ["core.norg.completion"] = {
+      ["core.defaults"] = {},  -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.keybinds"] = {},  -- Adds default keybindings
+      ["core.completion"] = {
         config = {
           engine = "nvim-cmp",
         },
-      }, -- Enables support for completion plugins
-      ["core.norg.journal"] = {}, -- Enables support for the journal module
-      ["core.norg.dirman"] = { -- Manages Neorg workspaces
+      },                     -- Enables support for completion plugins
+      ["core.journal"] = {}, -- Enables support for the journal module
+      ["core.dirman"] = {    -- Manages Neorg workspaces
         config = {
           workspaces = {
             work = "~/Neorg/work",
