acf248b1e9a51bf261ab5cb37959085fd927e317
iqbal1975 <iqbal.thaker@gmail.com>
2023-09-27 11:28:39 +0530

    Update AstroNvim configuration

--- options.lua
+++ options.lua
@@ -1,46 +1,36 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
-local opt = {
-  -- set to true or false etc.
-  colorcolumn = "90",
-  completeopt = { "menuone", "noinsert", "noselect" },
-  listchars = {
-    -- list of hidden characters
-    tab = "» ",
-    trail = "•",
-    precedes = "←",
-    extends = "→",
-    eol = "↲",
-    space = "␣",
-  },
-  nrformats = { "alpha", "octal", "hex" },
-  showbreak = "↪",
-  splitbelow = true,
-  splitright = true,
-  statuscolumn = "%l %r",
-  number = true, -- sets vim.opt.number
-  relativenumber = true, -- sets vim.opt.relativenumber
-  signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-  spell = false, -- sets vim.opt.spell
-  wrap = false, -- sets vim.opt.wrap
+-- set to true or false etc.
+-- Global
+vim.g.mapleader = " " -- sets vim.g.mapleader
+vim.g.autoformat_enabled = true -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+vim.g.cmp_enabled = true -- enable completion at start
+vim.g.autopairs_enabled = true -- enable autopairs at start
+vim.g.diagnostics_mode = 3 -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+vim.g.icons_enabled = true -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+vim.g.ui_notifications_enabled = true -- disable notifications when toggling UI elements
+
+-- Optional
+vim.opt.colorcolumn = "90"
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt.listchars = {
+  -- list of hidden characters
+  tab = "» ",
+  trail = "•",
+  precedes = "←",
+  extends = "→",
+  eol = "↲",
+  space = "␣",
 }
-
-for k, v in pairs(opt) do
-  vim.opt[k] = v
-end
-
-local og = {
-  mapleader = " ", -- sets vim.g.mapleader
-  autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-  cmp_enabled = true, -- enable completion at start
-  autopairs_enabled = true, -- enable autopairs at start
-  diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-  icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-  ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-}
-
-for k, v in pairs(og) do
-  vim.g[k] = v
-end
+vim.opt.nrformats = { "alpha", "octal", "hex" }
+vim.opt.showbreak = "↪"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+-- vim.opt.statuscolumn = "%l %r"
+-- vim.opt.number = true -- sets vim.opt.number
+-- vim.opt.relativenumber = true -- sets vim.opt.relativenumber
+vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
+vim.opt.spell = false -- sets vim.opt.spell
+vim.opt.wrap = false -- sets vim.opt.wrap
 
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
