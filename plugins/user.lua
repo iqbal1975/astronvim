82c95fa917c93861f733d5585e646b326760af30
iqbal1975 <iqbal.thaker@gmail.com>
2024-03-29 13:08:46 +0530

    Update AstroNvim configuration

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,14 +8,79 @@ return {
     config = function() require("indent-tools").config {} end,
   },
 
+  -- { -- Trouble v2
+  --   "folke/trouble.nvim",
+  --   cmd = { "TroubleToggle", "Trouble" },
+  --   opts = {
+  --     use_diagnostic_signs = true,
+  --     action_keys = {
+  --       close = { "q", "<esc>" },
+  --       cancel = "<c-e>",
+  --     },
+  --   },
+  -- },
+
   {
     "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = {
-      use_diagnostic_signs = true,
-      action_keys = {
-        close = { "q", "<esc>" },
-        cancel = "<c-e>",
+    branch = "dev", -- IMPORTANT!
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+    modes = {
+      preview_float = {
+        mode = "diagnostics",
+        preview = {
+          type = "float",
+          relative = "editor",
+          border = "rounded",
+          title = "Preview",
+          title_pos = "center",
+          position = { 0, -2 },
+          size = { width = 0.3, height = 0.3 },
+          zindex = 200,
+        },
+      },
+    },
+    mydiags = {
+      mode = "diagnostics", -- inherit from diagnostics mode
+      filter = {
+        any = {
+          buf = 0, -- current buffer
+          {
+            severity = vim.diagnostic.severity.ERROR, -- errors only
+            -- limit to files in the current project
+            function(item) return item.filename:find(vim.loop.cwd(), 1, true) end,
+          },
+        },
       },
     },
   },
