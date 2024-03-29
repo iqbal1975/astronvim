1c1bd072d58a59094f0a35495fcd24a47a0fedbd
iqbal1975 <iqbal.thaker@gmail.com>
2024-03-29 13:27:46 +0530

    Update AstroNvim configuration

---
+++ plugins/trouble.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "folke/trouble.nvim",
+    branch = "dev", -- IMPORTANT!
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+    modes = {
+      preview_float = {
+        mode = "diagnostics",
+        preview = {
+          type = "float",
+          relative = "editor",
+          border = "rounded",
+          title = "Preview",
+          title_pos = "center",
+          position = { 0, -2 },
+          size = { width = 0.3, height = 0.3 },
+          zindex = 200,
+        },
+      },
+      cascade = {
+        mode = "diagnostics", -- inherit from diagnostics mode
+        filter = function(items)
+          local severity = vim.diagnostic.severity.HINT
+          for _, item in ipairs(items) do
+            severity = math.min(severity, item.severity)
+          end
+          return vim.tbl_filter(function(item) return item.severity == severity end, items)
+        end,
+      },
+    },
+    mydiags = {
+      mode = "diagnostics", -- inherit from diagnostics mode
+      filter = {
+        any = {
+          buf = 0, -- current buffer
+          {
+            severity = vim.diagnostic.severity.ERROR, -- errors only
+            -- limit to files in the current project
+            function(item) return item.filename:find(vim.loop.cwd(), 1, true) end,
+          },
+        },
+      },
+    },
+  },
+}
